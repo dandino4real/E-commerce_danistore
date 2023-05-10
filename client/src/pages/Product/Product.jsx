f58a1675e798f7f9ef44ded8441ee44ab4fb1802
mac <mac@IMCMSILLNBCYF01.local>
2023-05-10 06:02:47 -0700

    first commit

---
+++ client/src/pages/Product/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Product.scss'
+
+const Product = () => {
+  return (
+    <div>
+      <span>Product</span>
+    </div>
+  )
+}
+
+export default Product
---
+++ client/src/pages/Products/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Products.scss"
+
+const Products = () => {
+  return (
+    <div>
+      <span>Category</span>
+    </div>
+  )
+}
+
+export default Products
